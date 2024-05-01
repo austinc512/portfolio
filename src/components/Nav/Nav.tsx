717183967b085d363ee4bdc73eeca1f7c52703fc
austinc512 <cello.austin1994@gmail.com>
2024-04-30 19:51:35 -0500

    starting on nav bar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,9 +4,9 @@ const Nav = () => {
   return (
     <nav>
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        <li>option 1</li>
+        <li>option 2</li>
+        <li>option 3</li>
       </ul>
     </nav>
   );
