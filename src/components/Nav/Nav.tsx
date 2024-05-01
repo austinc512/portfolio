12243f099a1df852c55e6d062feec634a7eea7c6
austinc512 <cello.austin1994@gmail.com>
2024-04-30 19:13:42 -0500

    starting new portfolio with React/TS/Vite

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,14 @@
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </nav>
+  );
+};
+export default Nav;
