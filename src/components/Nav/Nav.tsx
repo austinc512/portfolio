e5db5842dc08d9529dff3d30ca29983a9c5cb3ae
austinc512 <cello.austin1994@gmail.com>
2024-05-10 19:11:03 -0500

    working on nav, added SVGs

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,6 @@
 import './Nav.css';
+import linkedIn from '../../assets/linkedin.svg';
+import github from '../../assets/iconmonstr-github-3.svg';
 
 const Nav = () => {
   return (
@@ -10,6 +12,19 @@ const Nav = () => {
         <li className="item">Projects</li>
         <li className="item">Contact</li>
         <li className="item">Resume</li>
+        <li className="item">
+          <a
+            href="https://www.linkedin.com/in/austin-covey-a49075279"
+            target="_blank"
+          >
+            <img src={linkedIn} alt="linkedIn" />
+          </a>
+        </li>
+        <li className="item">
+          <a href="https://github.com/austinc512" target="_blank">
+            <img src={github} alt="github" />
+          </a>
+        </li>
       </ul>
     </nav>
   );
