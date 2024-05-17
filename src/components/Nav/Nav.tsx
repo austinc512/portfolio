4054d7a1a7ae9aaabff8f3b33337b1130065294d
austinc512 <cello.austin1994@gmail.com>
2024-05-17 17:10:58 -0500

    still working on navbar, will likely take a different approach on next commit

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './Nav.css';
 import linkedIn from '../../assets/linkedin.svg';
-import github from '../../assets/iconmonstr-github-3.svg';
+import github from '../../assets/github.svg';
+import menu from '../../assets/menu.svg';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="nav">
       <h1 className="name">Austin Covey</h1>
-      <ul className="navlist">
+      <div
+        className={`menu-toggle ${isOpen ? 'open' : ''}`}
+        onClick={toggleMenu}
+      >
+        {/* <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div> */}
+        <img className="menu-icon" src={menu} alt="" />
+      </div>
+      <ul className={`navlist ${isOpen ? 'open' : ''}`}>
         <li className="item">About</li>
         <li className="item">Skills</li>
         <li className="item">Projects</li>
